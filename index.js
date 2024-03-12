7aaceecc79ab78adebb26d7bde7b2b7f9367a145
LEVIS RABAH <levisrabah@gmail.com>
2024-03-12 08:57:34 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const name = "Joe";
-const height = 74;
+const { expect } = require("chai");
+
+const name = "Susan";
+const height = 39;
+describe("Height", () => {
+    
+        expect(height) > (0)
+        expect(height) < (39);
+  });
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
